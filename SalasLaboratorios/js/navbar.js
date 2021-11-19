24a9efc53497e92f0d8387f937a0087d39605f53
Augusto Pereira Aguiar <78914937+AugustoPAguiar@users.noreply.github.com>
2021-11-19 18:36:10 -0300

    Add files via upload

---
+++ SalasLaboratorios/js/navbar.js
@@ -0,0 +1,59 @@
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+      });
+    }
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+  );
+  mobileNavbar.init();
+
+
+function Relogio(){
+    var data = new Date();
+    var dia = data.getDay();
+    var mes = data.getMonth();
+    var ano = data.getFullYear();
+    var hora = data.getHours();
+    var minuto = data.getMinutes();
+    var segundos = data.getSeconds();
+
+    var horario = dia+"/"+mes+"/"+ano+"  -  "+hora+":"+minuto+":"+segundos;
+
+    var tempo = window.document.getElementById('horas');
+    tempo.innerHTML = horario;
+}
+setInterval(Relogio,500);
