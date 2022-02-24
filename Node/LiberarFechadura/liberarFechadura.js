cb1220f073dc198935f4b28263d94c53756ce0ed
JoaoKlug <klugteixeira@gmail.com>
2022-02-23 22:46:54 -0300

    liberarFechadura completo

--- Node/LiberarFechadura/liberarFechadura.js
+++ Node/LiberarFechadura/liberarFechadura.js
@@ -1,4 +1,4 @@
-var converterCartao =  require('../converterCartao/converterCartao');
+var converterCartao =  require('./converterCartao');
 //CONFIGURA O BD
 const Client = require('pg').Client;
 const cliente = new Client({
@@ -14,36 +14,29 @@ async function LiberarFechadura(hexCardCode)
     var nome_pessoa = "";
     var cracha_pessoa = 0;
     var acesso = false;
-    var id_cargo_pessoa = 0;
-    cracha_pessoa = converterCartao.ConverterCartao(hexCardCode);
-    console.log(cracha_pessoa);
+    var ativo = "";
+    cracha_pessoa = converterCartao(hexCardCode);
 
     try{
         console.log("Iniciando a conexão")
         await cliente.connect()
         console.log("Conexão bem sucedida")
 
-        const resultado = await cliente.query("select id_cargo_pessoa from tbPessoa where cracha_pessoa = '" + cracha_pessoa + "';")
-        var cargo_id = resultado.rows[0];                                                                                                                                                                                                                                                      ;
-        console.log(cargo_id);                                              
-        
-        if(cargo_id == 2){
-            acesso = true;
-            nome_pessoa = await cliente.query("select nome_pessoa from tbPessoa where cracha_pessoa = '" + cracha_pessoa + "';")
-            console.log(nome_pessoa);
-            const resultado = await cliente.query("select Cast(id_cargo_pessoa as INTEGER) from tbPessoa where cracha_pessoa = '" + cracha_pessoa + "';")
-            id_cargo_pessoa = resultado.getInt("id_cargo_pessoa");
-            console.log(id_cargo_pessoa);
-            const nome_cargo = await cliente.query("select nome_cargo from tbCargo where id_cargo = '" + id_cargo_pessoa + "';")
-            console.log(nome_cargo);
-            if (nome_cargo == "professor" || nome_cargo == "servidor")
+        var resultado = await cliente.query("select ativo from tbPessoa where cracha_pessoa = '" + cracha_pessoa + "';")
+            var ativo = resultado.rows[0].ativo;
+
+        resultado = await cliente.query("select id_cargo_pessoa from tbPessoa where cracha_pessoa = '" + cracha_pessoa + "';")
+            var id_cargo_pessoa = Number(resultado.rows[0].id_cargo_pessoa);
+
+        resultado = await cliente.query("select nome_cargo from tbCargo where id = '" + id_cargo_pessoa + "';")
+            var nome_cargo = resultado.rows[0].nome_cargo;
+
+            if (nome_cargo == "professor" || nome_cargo == "servidor" && ativo == "S")
             {
                 acesso = true;
-                nome_pessoa = await cliente.query("select nome_pessoa from tbPessoa where cracha_pessoa '" + cracha_pessoa + "';")
-                return nome_pessoa;        
+                resultado = await cliente.query("select nome_pessoa from tbPessoa where cracha_pessoa = '" + cracha_pessoa + "';")
+                    nome_pessoa = resultado.rows[0].nome_pessoa;     
             }
-        return acesso;
-        }
     }
     catch{
         console.log("Ocorreu um erro")
@@ -51,7 +44,14 @@ async function LiberarFechadura(hexCardCode)
     finally{
         await cliente.end()
         console.log("Cliente desconectado")
+        
     }
+    return {acesso, nome_pessoa};
 }
-var acesso = LiberarFechadura("590046E4BF");
-console.log(acesso);
+
+var acessoPromisse = LiberarFechadura("590046E4BF");
+acessoPromisse.then( function(acessoPromisse){
+    var acesso = acessoPromisse.acesso;
+    var nome_pessoa = acessoPromisse.nome_pessoa;
+    console.log(acesso + " " + nome_pessoa);
+});
