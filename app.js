4b6ae8ae58dbdfe3fc8ae317689d9db85b0e81e8
pedroDecolin <ph.filla@gmail.com>
2022-03-01 18:05:56 -0300

    remoção de código inutilizado

--- app.js
+++ app.js
@@ -157,51 +157,6 @@ app.post('/reqFechadura', (req, res) =>{
     console.log('enviado para o script acesso')
 })
 
-/*app.get('/info/get', (req, res) =>{
-    try{
-        pool.connect(async (error, client, release) =>{
-            let resp = await client.query(`select * from public.tbTeste;`)
-            release()
-            res.send(resp.rows)
-        })
-    }catch{
-        console.log(error)
-    }
-})
-
-app.post('/info/add', (req, res) =>{
-    try{
-        pool.connect(async (error, client, release) =>{
-            let resp = await client.query(`insert into tbteste(text) values('${req.body.add}');`)
-            res.redirect('/info/get')
-        })
-    }catch{
-        console.log(error)
-    }
-})
-
-app.post('/info/del', (req, res) =>{
-    try{
-        pool.connect(async (error, client, release) =>{
-            let resp = await client.query(`delete from tbteste where text = '${req.body.delete}'`)
-            res.redirect('/info/get')
-        })
-    }catch{
-        console.log(error)
-    }
-})
-
-app.post('/info/upd', (req, res) =>{
-    try{
-        pool.connect(async (error, client, release) =>{
-            let resp = await client.query(`UPDATE tbteste SET text = '${req.body.newValue}' WHERE text = '${req.body.oldValue}';`)
-            res.redirect('/info/get')
-        })
-    }catch{
-        console.log(error)
-    }
-})*/
-
 app.listen(port, () => { // faz com que o servidor rode na porta especificada
     console.log('server started on port ' + port)
 })
