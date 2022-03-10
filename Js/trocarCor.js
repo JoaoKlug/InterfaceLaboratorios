8ea201de58d729099f266056aef9e18ab032a43e
JoaoKlug <klugteixeira@gmail.com>
2022-03-09 22:23:20 -0300

    update TrocarCor

--- Js/trocarCor.js
+++ Js/trocarCor.js
@@ -1,9 +1,13 @@
-var estado_fechadura = false
-console.log(estado_fechadura);
-var element = document.getElementById("estadofechadura");
+module.exports = function trocarCor(estado_fechadura)
+ {  
+    console.log(estado_fechadura);
+    
+    var element = document.getElementById("estadofechadura");
+    
+    if (estado_fechadura == false){
+        element.style.backgroundColor = "#FF0000";
+    }else{
+        element.style.backgroundColor = "#00ff00";
+    }
+ }
 
-if (estado_fechadura == false){
-    element.style.backgroundColor = "#FF0000";
-}else{
-    element.style.backgroundColor = "#00ff00";
-}
